eaff7df682273fc49cb3f85d2117afbf702b891d
Martin <m4rtin.t@gmail.com>
2021-10-17 15:40:02 -0400

    Tests for Character Dash

--- Assets/_BForBoss/_Character/Scripts/FirstPersonPlayer.cs
+++ Assets/_BForBoss/_Character/Scripts/FirstPersonPlayer.cs
@@ -144,6 +144,16 @@ namespace BForBoss
             }
         }
 
+        protected override void OnOnDestroy()
+        {
+            base.OnOnDestroy();
+            if (_dashBehaviour != null)
+            {
+                _dashBehaviour.OnOnDestroy();
+            }
+        }
+
+
         #region Helper
 
         private bool IsSliding()
--- Assets/_BForBoss/_Character/Scripts/PlayerDashBehaviour.cs
+++ Assets/_BForBoss/_Character/Scripts/PlayerDashBehaviour.cs
@@ -155,6 +155,13 @@ namespace BForBoss
             _dashInputAction.Disable();
         }
 
+        public void OnOnDestroy()
+        {
+            _dashInputAction.started -= OnDash;
+            _dashInputAction.canceled -= OnDash;
+            _dashInputAction = null;
+        }
+
         #endregion
     }
 }
