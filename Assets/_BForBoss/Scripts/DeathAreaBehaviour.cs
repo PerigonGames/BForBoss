7a6cea9d23f17640f08ce25f826ef6ab9030c60c
Michael Goh <mikegoh1997@gmail.com>
2021-10-26 11:44:52 -0700

    Refactor to use static methods
    - move all event related code to PerigonAnalytics

--- Assets/_BForBoss/Scripts/DeathAreaBehaviour.cs
+++ Assets/_BForBoss/Scripts/DeathAreaBehaviour.cs
@@ -6,22 +6,10 @@ namespace BForBoss
 {
     public class DeathAreaBehaviour : MonoBehaviour
     {
-        private const String PlayerDeathEvent = "Player - Death";
         private void OnCollisionEnter(Collision other)
         {
             StateManager.Instance.SetState(State.Death);
-            SendPlayerDeathEvent();
-        }
-
-        private void SendPlayerDeathEvent()
-        {
-            PerigonAnalytics perigonAnalytics = new PerigonAnalytics();
-            
-            Hashtable parameters = new Hashtable();
-            parameters.Add("course", "racecourse");
-            parameters.Add("name", gameObject.name);
-
-            perigonAnalytics.LogEventWithParams(PlayerDeathEvent, parameters);
+            PerigonAnalytics.LogDeathEvent("racecourse", gameObject.name);        
         }
     }
 }
--- Assets/_BForBoss/_Analytics/Scripts/AnalyticsTracker.cs
+++ Assets/_BForBoss/_Analytics/Scripts/AnalyticsTracker.cs
@@ -5,21 +5,15 @@ namespace BForBoss
 {
     public class AnalyticsTracker : MonoBehaviour
     {
-        private const String SessionStart = "Session Start";
-        private const String SessionEnd = "Session End";
-        private PerigonAnalytics _perigonAnalytics = new PerigonAnalytics();
-        
         private void Start()
         {
-            _perigonAnalytics.LogEvent(SessionStart);
+            PerigonAnalytics.StartSession();
         }
 
         // this also works in Editor when playmode is stopped
         private void OnApplicationQuit()
         {
-            _perigonAnalytics.LogEvent(SessionEnd);
-            // before the game ends, make sure any analytics collected is sent to Mixpanel
-            _perigonAnalytics.ForceSendEvents();
+            PerigonAnalytics.EndSession();
         }
     }
 }
