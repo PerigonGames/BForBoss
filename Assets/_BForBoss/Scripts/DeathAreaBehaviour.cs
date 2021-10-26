3dc2f17784a336e4d40066697d436db70622155b
Michael Goh <mikegoh1997@gmail.com>
2021-10-26 11:21:30 -0700

    Send player death event to MixPanel

--- Assets/_BForBoss/Scripts/DeathAreaBehaviour.cs
+++ Assets/_BForBoss/Scripts/DeathAreaBehaviour.cs
@@ -1,12 +1,27 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 namespace BForBoss
 {
     public class DeathAreaBehaviour : MonoBehaviour
     {
+        private const String PlayerDeathEvent = "Player - Death";
         private void OnCollisionEnter(Collision other)
         {
             StateManager.Instance.SetState(State.Death);
+            SendPlayerDeathEvent();
+        }
+
+        private void SendPlayerDeathEvent()
+        {
+            PerigonAnalytics perigonAnalytics = new PerigonAnalytics();
+            
+            Hashtable parameters = new Hashtable();
+            parameters.Add("course", "racecourse");
+            parameters.Add("name", gameObject.name);
+
+            perigonAnalytics.LogEventWithParams(PlayerDeathEvent, parameters);
         }
     }
 }
