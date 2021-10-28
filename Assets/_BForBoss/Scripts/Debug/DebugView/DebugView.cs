cad3e0aa8cb9f88e6836ea5bfb6505e8d6df6243
Gursaanj <gursaanjresides@gmail.com>
2021-10-28 03:50:24 -0700

    Update DebugView.cs

--- Assets/_BForBoss/Scripts/Debug/DebugView/DebugView.cs
+++ Assets/_BForBoss/Scripts/Debug/DebugView/DebugView.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public abstract class DebugView
 {
+    protected Rect _masterRect;
+    protected Rect _baseRect;
+    protected Rect _backButtonRect;
+
+    private const float _backButtonHeight = 20f;
     
+    public Rect MasterRect
+    {
+        set
+        {
+            _masterRect = value;
+            CreateBaseRect();
+        }
+    }
+
+    protected abstract void DrawWindow(int id);
+
+    protected virtual void CreateBaseRect()
+    {
+        
+    }
 }
