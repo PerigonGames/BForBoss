696fa4d4624693012ae27290ec4ef97a1e891e9b
AbelW <unoctium1@gmail.com>
2021-11-08 12:37:20 -0800

    Move into seperate file

--- Assets/_BForBoss/Tests/CharacterThirdPersonSlideTests.cs
+++ Assets/_BForBoss/Tests/CharacterThirdPersonSlideTests.cs
@@ -1,7 +1,7 @@
-using System;
-using System.Collections;
 using BForBoss;
 using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,7 +10,7 @@ using UnityEngine.TestTools;
 
 namespace Tests.Character
 {
-    public class CharacterSlideTests : InputTestFixture
+    public class CharacterThirdPersonSlideTests : InputTestFixture
     {
         private Keyboard _keyboard = null;
 
@@ -21,246 +21,218 @@ namespace Tests.Character
             EditorSceneManager.LoadSceneAsyncInPlayMode("Assets/_BForBoss/Tests/Scenes/CharacterSlideTest.unity", new LoadSceneParameters(LoadSceneMode.Single));
             _keyboard = InputSystem.AddDevice<Keyboard>();
         }
-        
+
         [UnityTest]
-        public IEnumerator Character_Slide_StayingStill()
+        public IEnumerator CharacterThirdPerson_CrouchLeft_BeyondLeftBlockade()
         {
             for (int i = 0; i < 5; i++)
             {
                 yield return new WaitForFixedUpdate();
             }
-            
-            var originalPosition = Vector3.zero;
-            var character = GameObject.FindObjectOfType<FirstPersonPlayer>();
-            character.transform.position = originalPosition;
-            
-            Press(_keyboard.cKey);
-            yield return new WaitForSeconds(1.0f);
 
-            var withinBoundsXPosition = TestUtilities.WithinBounds(character.transform.position.x, 0);
-            var withinBoundsYPosition = TestUtilities.WithinBounds(character.transform.position.z, 0);
-
-            Assert.IsTrue(withinBoundsXPosition, "Crouching should not have moved player when standing still");
-            Assert.IsTrue(withinBoundsYPosition, "Crouching should not have moved player when standing still");
-        }
-        
-        [UnityTest]
-        public IEnumerator Character_JumpCrouch_StayingStill()
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                yield return new WaitForFixedUpdate();
-            }
-            
-            var originalPosition = Vector3.zero;
-            var character = GameObject.FindObjectOfType<FirstPersonPlayer>();
-            character.transform.position = originalPosition;
-            
-            Press(_keyboard.spaceKey);
-            yield return new WaitForSeconds(0.25f);
-            Press(_keyboard.cKey);
-            yield return new WaitForSeconds(1.0f);
-
-            var withinBoundsXPosition = TestUtilities.WithinBounds(character.transform.position.x, 0);
-            var withinBoundsYPosition = TestUtilities.WithinBounds(character.transform.position.z, 0);
-
-            Assert.IsTrue(withinBoundsXPosition, "Crouching should not have moved player when standing still");
-            Assert.IsTrue(withinBoundsYPosition, "Crouching should not have moved player when standing still");
-        }
-        
-        [UnityTest]
-        public IEnumerator Character_CrouchLeft_BeyondLeftBlockade()
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                yield return new WaitForFixedUpdate();
-            }
-            
             var originalPosition = Vector3.zero;
             var character = GameObject.FindObjectOfType<FirstPersonPlayer>();
             var blockadeLeft = GameObject.Find("Blockade_Left");
             character.transform.position = originalPosition;
-            
+
+            Press(_keyboard.qKey);
+            yield return new WaitForSeconds(0.25f);
             Press(_keyboard.cKey);
-            yield return  new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(0.5f);
             Press(_keyboard.aKey);
-            yield return  new WaitForSeconds(2f);
+            yield return new WaitForSeconds(2f);
 
 
             Assert.Less(character.transform.position.x, blockadeLeft.transform.position.x, "Crouching left should have went under and beyond the left blockade");
         }
-        
+
         [UnityTest]
-        public IEnumerator Character_CrouchRight_BeyondRightBlockade()
+        public IEnumerator CharacterThirdPerson_CrouchRight_BeyondRightBlockade()
         {
             for (int i = 0; i < 5; i++)
             {
                 yield return new WaitForFixedUpdate();
             }
-            
+
             var originalPosition = Vector3.zero;
             var character = GameObject.FindObjectOfType<FirstPersonPlayer>();
             var blockadeLeft = GameObject.Find("Blockade_Right");
             character.transform.position = originalPosition;
-            
+
+            Press(_keyboard.qKey);
+            yield return new WaitForSeconds(0.25f);
             Press(_keyboard.cKey);
             yield return new WaitForSeconds(0.5f);
             Press(_keyboard.dKey);
-            yield return  new WaitForSeconds(2f);
+            yield return new WaitForSeconds(2f);
 
 
             Assert.Greater(character.transform.position.x, blockadeLeft.transform.position.x, "Crouching right should have went under and beyond the right blockade");
         }
-        
+
         [UnityTest]
-        public IEnumerator Character_CrouchForward_BeyondForwardBlockade()
+        public IEnumerator CharacterThirdPerson_CrouchForward_BeyondForwardBlockade()
         {
             for (int i = 0; i < 5; i++)
             {
                 yield return new WaitForFixedUpdate();
             }
-            
+
             var originalPosition = Vector3.zero;
             var character = GameObject.FindObjectOfType<FirstPersonPlayer>();
             var blockadeForward = GameObject.Find("Blockade_Forward");
             character.transform.position = originalPosition;
-            
+
+            Press(_keyboard.qKey);
+            yield return new WaitForSeconds(0.25f);
             Press(_keyboard.cKey);
             yield return new WaitForSeconds(0.5f);
             Press(_keyboard.wKey);
-            yield return  new WaitForSeconds(2);
+            yield return new WaitForSeconds(2);
 
 
             Assert.Greater(character.transform.position.z, blockadeForward.transform.position.z, "Crouching forward should have went under and beyond the forward blockade");
         }
-        
+
         [UnityTest]
-        public IEnumerator Character_CrouchBackward_BeyondBackwardBlockade()
+        public IEnumerator CharacterThirdPerson_CrouchBackward_BeyondBackwardBlockade()
         {
             for (int i = 0; i < 5; i++)
             {
                 yield return new WaitForFixedUpdate();
             }
-            
+
             var originalPosition = Vector3.zero;
             var character = GameObject.FindObjectOfType<FirstPersonPlayer>();
             var blockadeForward = GameObject.Find("Blockade_Backward");
             character.transform.position = originalPosition;
-            
+
+            Press(_keyboard.qKey);
+            yield return new WaitForSeconds(0.25f);
             Press(_keyboard.cKey);
             yield return new WaitForSeconds(0.5f);
             Press(_keyboard.sKey);
-            yield return  new WaitForSeconds(2);
+            yield return new WaitForSeconds(2);
 
 
             Assert.Less(character.transform.position.z, blockadeForward.transform.position.z, "Crouch backward should have went under and beyond the backward blockade");
         }
-        
+
         [UnityTest]
-        public IEnumerator Character_SlideLeft_BeyondLeftBlockade()
+        public IEnumerator CharacterThirdPerson_SlideLeft_BeyondLeftBlockade()
         {
             for (int i = 0; i < 5; i++)
             {
                 yield return new WaitForFixedUpdate();
             }
-            
+
             var originalPosition = Vector3.zero;
             var character = GameObject.FindObjectOfType<FirstPersonPlayer>();
             var blockadeLeft = GameObject.Find("Blockade_Left");
             character.transform.position = originalPosition;
-            
+
+            Press(_keyboard.qKey);
+            yield return new WaitForSeconds(0.25f);
             Press(_keyboard.aKey);
-            yield return  new WaitForSeconds(0);
+            yield return new WaitForSeconds(0);
             Press(_keyboard.cKey);
             yield return new WaitForSeconds(1.0f);
 
 
             Assert.Less(character.transform.position.x, blockadeLeft.transform.position.x, "Sliding left should have went under and beyond the left blockade");
         }
-        
+
         [UnityTest]
-        public IEnumerator Character_SlideRight_BeyondRightBlockade()
+        public IEnumerator CharacterThirdPerson_SlideRight_BeyondRightBlockade()
         {
             for (int i = 0; i < 5; i++)
             {
                 yield return new WaitForFixedUpdate();
             }
-            
+
             var originalPosition = Vector3.zero;
             var character = GameObject.FindObjectOfType<FirstPersonPlayer>();
             var blockadeLeft = GameObject.Find("Blockade_Right");
             character.transform.position = originalPosition;
-            
+
+            Press(_keyboard.qKey);
+            yield return new WaitForSeconds(0.25f);
             Press(_keyboard.dKey);
-            yield return  new WaitForSeconds(0);
+            yield return new WaitForSeconds(0);
             Press(_keyboard.cKey);
             yield return new WaitForSeconds(1.0f);
 
 
             Assert.Greater(character.transform.position.x, blockadeLeft.transform.position.x, "Sliding right should have went under and beyond the right blockade");
         }
-        
+
         [UnityTest]
-        public IEnumerator Character_SlideForward_BeyondForwardBlockade()
+        public IEnumerator CharacterThirdPerson_SlideForward_BeyondForwardBlockade()
         {
             for (int i = 0; i < 5; i++)
             {
                 yield return new WaitForFixedUpdate();
             }
-            
+
             var originalPosition = Vector3.zero;
             var character = GameObject.FindObjectOfType<FirstPersonPlayer>();
             var blockadeForward = GameObject.Find("Blockade_Forward");
             character.transform.position = originalPosition;
-            
+
+            Press(_keyboard.qKey);
+            yield return new WaitForSeconds(0.25f);
             Press(_keyboard.wKey);
-            yield return  new WaitForSeconds(0);
+            yield return new WaitForSeconds(0);
             Press(_keyboard.cKey);
             yield return new WaitForSeconds(1.0f);
 
 
             Assert.Greater(character.transform.position.z, blockadeForward.transform.position.z, "Sliding forward should have went under and beyond the forward blockade");
         }
-        
+
         [UnityTest]
-        public IEnumerator Character_SlideBackward_BeyondBackwardBlockade()
+        public IEnumerator CharacterThirdPerson_SlideBackward_BeyondBackwardBlockade()
         {
             for (int i = 0; i < 5; i++)
             {
                 yield return new WaitForFixedUpdate();
             }
-            
+
             var originalPosition = Vector3.zero;
             var character = GameObject.FindObjectOfType<FirstPersonPlayer>();
             var blockadeForward = GameObject.Find("Blockade_Backward");
             character.transform.position = originalPosition;
-            
+
+            Press(_keyboard.qKey);
+            yield return new WaitForSeconds(0.25f);
             Press(_keyboard.sKey);
-            yield return  new WaitForSeconds(0);
+            yield return new WaitForSeconds(0);
             Press(_keyboard.cKey);
             yield return new WaitForSeconds(1.0f);
 
 
             Assert.Less(character.transform.position.z, blockadeForward.transform.position.z, "Sliding backward should have went under and beyond the backward blockade");
         }
-        
+
         [UnityTest]
-        public IEnumerator Character_SlideForwardRight_BeyondDiagonalForwardRightBlockade()
+        public IEnumerator CharacterThirdPerson_SlideForwardRight_BeyondDiagonalForwardRightBlockade()
         {
             for (int i = 0; i < 5; i++)
             {
                 yield return new WaitForFixedUpdate();
             }
-            
+
             var originalPosition = Vector3.zero;
             var character = GameObject.FindObjectOfType<FirstPersonPlayer>();
             var blockadeForward = GameObject.Find("Blockade_Forward");
             var blockadeRight = GameObject.Find("Blockade_Right");
             character.transform.position = originalPosition;
-            
+
+            Press(_keyboard.qKey);
+            yield return new WaitForSeconds(0.25f);
             Press(_keyboard.wKey);
             Press(_keyboard.dKey);
-            yield return  new WaitForSeconds(0);
+            yield return new WaitForSeconds(0);
             Press(_keyboard.cKey);
             yield return new WaitForSeconds(1.0f);
 
@@ -268,24 +240,26 @@ namespace Tests.Character
             Assert.Greater(character.transform.position.z, blockadeForward.transform.position.z, "Sliding forward right should have went under and beyond the forward blockade");
             Assert.Greater(character.transform.position.x, blockadeRight.transform.position.x, "Sliding forward right should have went under and beyond the right blockade");
         }
-        
+
         [UnityTest]
-        public IEnumerator Character_SlideForwardLeft_BeyondDiagonalForwardLeftBlockade()
+        public IEnumerator CharacterThirdPerson_SlideForwardLeft_BeyondDiagonalForwardLeftBlockade()
         {
             for (int i = 0; i < 5; i++)
             {
                 yield return new WaitForFixedUpdate();
             }
-            
+
             var originalPosition = Vector3.zero;
             var character = GameObject.FindObjectOfType<FirstPersonPlayer>();
             var blockadeForward = GameObject.Find("Blockade_Forward");
             var blockadeLeft = GameObject.Find("Blockade_Left");
             character.transform.position = originalPosition;
-            
+
+            Press(_keyboard.qKey);
+            yield return new WaitForSeconds(0.25f);
             Press(_keyboard.wKey);
             Press(_keyboard.aKey);
-            yield return  new WaitForSeconds(0);
+            yield return new WaitForSeconds(0);
             Press(_keyboard.cKey);
             yield return new WaitForSeconds(1.0f);
 
@@ -293,24 +267,26 @@ namespace Tests.Character
             Assert.Greater(character.transform.position.z, blockadeForward.transform.position.z, "Sliding forward left should have went under and beyond the forward blockade");
             Assert.Less(character.transform.position.x, blockadeLeft.transform.position.x, "Sliding forward left should have went under and beyond the left blockade");
         }
-        
+
         [UnityTest]
-        public IEnumerator Character_SlideBackwardLeft_BeyondDiagonalBackwardLeftBlockade()
+        public IEnumerator CharacterThirdPerson_SlideBackwardLeft_BeyondDiagonalBackwardLeftBlockade()
         {
             for (int i = 0; i < 5; i++)
             {
                 yield return new WaitForFixedUpdate();
             }
-            
+
             var originalPosition = Vector3.zero;
             var character = GameObject.FindObjectOfType<FirstPersonPlayer>();
             var blockadeBackward = GameObject.Find("Blockade_Backward");
             var blockadeLeft = GameObject.Find("Blockade_Left");
             character.transform.position = originalPosition;
-            
+
+            Press(_keyboard.qKey);
+            yield return new WaitForSeconds(0.25f);
             Press(_keyboard.sKey);
             Press(_keyboard.aKey);
-            yield return  new WaitForSeconds(0);
+            yield return new WaitForSeconds(0);
             Press(_keyboard.cKey);
             yield return new WaitForSeconds(1.0f);
 
@@ -319,8 +295,7 @@ namespace Tests.Character
             Assert.Less(character.transform.position.x, blockadeLeft.transform.position.x, "Sliding backward left should have went under and beyond the left blockade");
         }
 
-        /*
-         THIS IS CURRENTLY BUGGED
+        /* Currently bugged
         [UnityTest]
         public IEnumerator Character_SlideBackwardRight_BeyondDiagonalBackwardRightBlockade()
         {
@@ -328,24 +303,24 @@ namespace Tests.Character
             {
                 yield return new WaitForFixedUpdate();
             }
-            
+
             var originalPosition = Vector3.zero;
             var character = GameObject.FindObjectOfType<FirstPersonPlayer>();
             var blockadeBackward = GameObject.Find("Blockade_Backward");
             var blockadeRight = GameObject.Find("Blockade_Right");
             character.transform.position = originalPosition;
-            
+
+            Press(_keyboard.qKey);
+            yield return new WaitForSeconds(0.25f);s
             Press(_keyboard.sKey);
             Press(_keyboard.dKey);
-            yield return  new WaitForSeconds(0);
+            yield return new WaitForSeconds(0);
             Press(_keyboard.cKey);
             yield return new WaitForSeconds(1.0f);
-            
+
             Assert.Less(character.transform.position.z, blockadeBackward.transform.position.z, "Sliding backward right should have went under and beyond the forward blockade");
             Assert.Greater(character.transform.position.x, blockadeRight.transform.position.x, "Sliding backward right should have went under and beyond the right blockade");
         }
         */
-        
     }
 }
-
