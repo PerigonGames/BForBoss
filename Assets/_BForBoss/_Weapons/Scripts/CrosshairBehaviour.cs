bd157f4ba1f81797bdc520bb8a96f16ed5120ae1
Martin <m4rtin.t@gmail.com>
2021-12-02 21:33:03 -0800

    Crosshair

---
+++ Assets/_BForBoss/_Weapons/Scripts/CrosshairBehaviour.cs
@@ -0,0 +1,28 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Perigon.Weapons
+{
+    public class CrosshairBehaviour : MonoBehaviour
+    {
+        [SerializeField] private Sprite _defaultCrosshair = null;
+        private Image _crosshair = null;
+        
+        private void Awake()
+        {
+            _crosshair = GetComponentInChildren<Image>();
+        }
+
+        private void Start()
+        {
+            _crosshair.sprite = _defaultCrosshair;
+        }
+
+        [Button]
+        public void SetCrosshairImage(Sprite image)
+        {
+            _crosshair.sprite = image;
+        }
+    }
+}
