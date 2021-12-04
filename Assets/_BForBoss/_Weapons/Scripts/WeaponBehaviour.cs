4a5f2e0a16ffff65f968f1f13e39ef4036063d80
Martin <m4rtin.t@gmail.com>
2021-12-03 17:02:08 -0800

    Added Semi Auto Rifle

--- Assets/_BForBoss/_Weapons/Scripts/WeaponBehaviour.cs
+++ Assets/_BForBoss/_Weapons/Scripts/WeaponBehaviour.cs
@@ -6,20 +6,34 @@ namespace Perigon.Weapons
 {
     public abstract class WeaponBehaviour : MonoBehaviour
     {
-
+        private readonly Vector3 CenterOfCameraPosition = new Vector3(0.5f, 0.5f, 0);
+        private readonly float FutherestDistanceToRayCast = 10000f;
         [SerializeField] private InputActionAsset _actions;
         [SerializeField] protected Transform _firePoint = null;
         [InlineEditor]
         [SerializeField] private WeaponScriptableObject _weaponScriptableObject;
        
-        private InputAction _fireInputAction { get; set; }
+        private InputAction FireInputAction { get; set; }
         private Camera _mainCamera = null;
         
         protected float _elapsedRateOfFire = 0;
  
         protected IWeapon _weaponProperty;
 
-        protected bool CanShoot => _elapsedRateOfFire < 0;
+        protected bool CanShoot => _elapsedRateOfFire <= 0;
+        
+        private Camera MainCamera
+        {
+            get
+            {
+                if (_mainCamera == null)
+                {
+                    _mainCamera = Camera.main;
+                }
+
+                return _mainCamera;
+            }
+        }
         
         public void Initialize(IWeapon weaponProperty = null)
         {
@@ -32,27 +46,35 @@ namespace Perigon.Weapons
         protected void Fire()
         {
             _elapsedRateOfFire = _weaponProperty.RateOfFire;
-            GenerateBullet(_firePoint.position);
+            GenerateBullet(_firePoint.position, GetDirectionOfShot());
         }
         
-        protected void GenerateBullet(Vector3 position)
+        //Placeholder
+        protected void GenerateBullet(Vector3 position, Vector3 fireDirection)
         {
             var bullet = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            bullet.GetComponent<Collider>().enabled = false;
+            bullet.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+            var rb = bullet.AddComponent<Rigidbody>();
+            rb.AddForce(fireDirection * 500, ForceMode.Impulse);
+            
             bullet.transform.position = position;
         }
 
-        private Camera MainCamera
+        private Vector3 GetDirectionOfShot()
         {
-            get
+            var camRay = MainCamera.ViewportPointToRay(CenterOfCameraPosition);
+            Vector3 targetPoint; 
+            if (Physics.Raycast(camRay, out var hit))
             {
-                if (_mainCamera == null)
-                {
-                    _mainCamera = Camera.main;
-                }
-
-                return _mainCamera;
+                targetPoint = hit.point;
             }
+            else
+            {
+                targetPoint = camRay.GetPoint(FutherestDistanceToRayCast);
+            }
+
+            var directionWithoutSpread = targetPoint - _firePoint.position;
+            return directionWithoutSpread.normalized;
         }
 
         private void Awake()
@@ -69,12 +91,12 @@ namespace Perigon.Weapons
                 return;
             }
             
-            _fireInputAction = _actions.FindAction("Fire");
-            if (_fireInputAction != null)
+            FireInputAction = _actions.FindAction("Fire");
+            if (FireInputAction != null)
             {
-                _fireInputAction.started += OnFire;
-                _fireInputAction.canceled += OnFire;
-                _fireInputAction.Enable();
+                FireInputAction.started += OnFire;
+                FireInputAction.canceled += OnFire;
+                FireInputAction.Enable();
             }
         }
 
