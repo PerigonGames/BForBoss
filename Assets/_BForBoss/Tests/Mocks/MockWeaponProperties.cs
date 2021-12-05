41bc036e8b0fab08d9fd1a040894bc03703baaee
Martin <m4rtin.t@gmail.com>
2021-12-05 13:03:40 -0800

    Fix tests

--- Assets/_BForBoss/Tests/Mocks/MockWeaponProperties.cs
+++ Assets/_BForBoss/Tests/Mocks/MockWeaponProperties.cs
@@ -7,16 +7,18 @@ namespace Tests
     {
         private float _rateOfFire;
         private float _bulletSpread;
+        private int _numberOfBullets;
         
         public float RateOfFire => _rateOfFire;
         public Sprite Crosshair => null;
         public float BulletSpread => _bulletSpread;
-        public int NumberOfBullets => 1;
+        public int NumberOfBullets => _numberOfBullets;
 
-        public MockWeaponProperties(float rateOfFire = 0.1f, float bulletSpread = 1f)
+        public MockWeaponProperties(float rateOfFire = 0.1f, float bulletSpread = 1f, int numberOfBullets = 1)
         {
             _rateOfFire = rateOfFire;
             _bulletSpread = bulletSpread;
+            _numberOfBullets = numberOfBullets;
         }
     }
 }
--- Assets/_BForBoss/Tests/Tools/TestUtilities.cs
+++ Assets/_BForBoss/Tests/Tools/TestUtilities.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine;
 
 namespace Tests
 {
@@ -7,6 +8,14 @@ namespace Tests
         public static bool WithinBounds(float firstArg, float secondArg, float bound = 0.1f)
         {
             return Math.Abs(firstArg - secondArg) < bound;
+        }
+        
+        public static bool WithinBounds(Vector3 firstArg, Vector3 secondArg, float bound = 0.1f)
+        {
+            var x = Math.Abs(firstArg.x - secondArg.x) < bound;
+            var y = Math.Abs(firstArg.y - secondArg.y) < bound;
+            var z = Math.Abs(firstArg.z - secondArg.z) < bound;
+            return x && y && z;
         }
     }  
 }
