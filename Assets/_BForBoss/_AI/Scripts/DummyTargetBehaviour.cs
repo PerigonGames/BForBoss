9172e6d7c42b1a900f6f96e769cdd22a8f6d7d7c
AbelW <unoctium1@gmail.com>
2021-12-10 14:21:57 -0800

    Add wip animations

--- Assets/_BForBoss/_AI/Scripts/DummyTargetBehaviour.cs
+++ Assets/_BForBoss/_AI/Scripts/DummyTargetBehaviour.cs
@@ -6,9 +6,42 @@ namespace Perigon.AI
 {
     public class DummyTargetBehaviour : LifeCycleBehaviour
     {
+        private const float DEATH_ANIMATION_DURATION = 5f;
+        private const string IS_DEAD = "IsDead";
+        private const string HIT = "Hit";
+        private readonly int DEATH_ID = Animator.StringToHash(IS_DEAD);
+        private readonly int HIT_ID = Animator.StringToHash(HIT);
+        
+        private Animator _animator;
+        
+        
+        protected override void Awake()
+        {
+            base.Awake();
+            _animator = GetComponentInChildren<Animator>();
+        }
+
         protected override void LifeCycleFinished()
         {
-            Destroy(gameObject);
+            _animator.SetBool(DEATH_ID, true);
+            Destroy(gameObject, DEATH_ANIMATION_DURATION);
+        }
+
+        private void TriggerHitAnimation()
+        {
+            _animator.SetTrigger(HIT_ID);
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            _lifeCycle.OnDamageTaken += TriggerHitAnimation;
+        }
+        
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            _lifeCycle.OnDamageTaken -= TriggerHitAnimation;
         }
     }
 }
