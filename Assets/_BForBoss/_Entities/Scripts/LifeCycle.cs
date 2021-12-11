3050932ab8d4b18f3951d9987c5f02b0d343b0e5
AbelW <unoctium1@gmail.com>
2021-12-10 16:02:25 -0800

    Prevent corpses from taking damage or dying twice, lol

--- Assets/_BForBoss/_Entities/Scripts/LifeCycle.cs
+++ Assets/_BForBoss/_Entities/Scripts/LifeCycle.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Perigon.Entities
@@ -10,6 +8,8 @@ namespace Perigon.Entities
         private readonly float _maxHealth;
         private float _currentHealth;
 
+        private bool _isAlive;
+        
         public event Action OnDamageTaken;
         public event Action OnHeal;
         public event Action OnDeath;
@@ -17,6 +17,7 @@ namespace Perigon.Entities
         public LifeCycle(IHealth health)
         {
             _maxHealth = _currentHealth = health.Health;
+            _isAlive = true;
         }
         
         public void HealBy(float amount)
@@ -27,10 +28,12 @@ namespace Perigon.Entities
 
         public void DamageBy(float amount)
         {
+            if (!_isAlive) return;
             _currentHealth -= amount;
             OnDamageTaken?.Invoke();
             if (_currentHealth <= 0f)
             {
+                _isAlive = false;
                 OnDeath?.Invoke();
             }
         }
