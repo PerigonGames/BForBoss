5af20cbcf36f8a33579404109a6ce0f75a19e70d
Martin <m4rtin.t@gmail.com>
2021-12-17 00:23:06 -0800

    Update InputSettingsViewModelTests.cs

--- Assets/_BForBoss/Tests/InputSettingsViewModelTests.cs
+++ Assets/_BForBoss/Tests/InputSettingsViewModelTests.cs
@@ -6,7 +6,7 @@ namespace Tests.Input
     public class InputSettingsViewModelTests
     {
         [Test]
-        public void Test_InputSettings_ConstructorSetup()
+        public void Test_InputSettings_MouseKeyboardConstructorSetup()
         {
             //Given
             var inputSettings = new MockInputSettings
@@ -22,12 +22,30 @@ namespace Tests.Input
             var viewModel = new MouseKeyboardInputSettingsViewModel(inputSettings, new MockAnalytics());
             
             //Then
-            /*
-            Assert.AreEqual(viewModel.GetMouseHorizontal, 0.1f, "Mouse Horizontal value should be the same as input settings");
-            Assert.AreEqual(viewModel.GetMouseVertical, 0.2f, "Mouse Vertical value should be the same as input settings");
-            Assert.AreEqual(viewModel.GetControllerHorizontal, 0.3f, "Controller Horizontal value should be the same as input settings");
-            Assert.AreEqual(viewModel.GetControllerVeritcal, 0.4f, "Controller Vertical value should be the same as input settings");
-            */
+            Assert.AreEqual(viewModel.GetHorizontal, 0.1f, "Mouse Horizontal value should be the same as input settings");
+            Assert.AreEqual(viewModel.GetVertical, 0.2f, "Mouse Vertical value should be the same as input settings");
+            Assert.IsTrue(viewModel.GetIsInverted, "Input should be inverted");
+        }
+        
+        [Test]
+        public void Test_InputSettings_ControllerConstructorSetup()
+        {
+            //Given
+            var inputSettings = new MockInputSettings
+            {
+                MouseHorizontalSensitivity = 0.1f,
+                MouseVerticalSensitivity = 0.2f,
+                ControllerHorizontalSensitivity = 0.3f,
+                ControllerVerticalSensitivity = 0.4f,
+                IsInverted = true
+            };
+
+            //When
+            var viewModel = new ControllerInputSettingsViewModel(inputSettings, new MockAnalytics());
+            
+            //Then
+            Assert.AreEqual(viewModel.GetHorizontal, 0.3f, "Controller Horizontal value should be the same as input settings");
+            Assert.AreEqual(viewModel.GetVertical, 0.4f, "Controller Vertical value should be the same as input settings");
             Assert.IsTrue(viewModel.GetIsInverted, "Input should be inverted");
         }
         
@@ -46,7 +64,7 @@ namespace Tests.Input
         }
         
         [Test]
-        public void Test_AppleSettings()
+        public void Test_MouseKeyboardApplySettings()
         {
             //Given
             var inputSettings = new MockInputSettings
@@ -60,13 +78,34 @@ namespace Tests.Input
             var viewModel = new MouseKeyboardInputSettingsViewModel(inputSettings, new MockAnalytics());
 
             //When
-            //viewModel.ApplySettings(9, 9, 9, 9, false);
+            viewModel.ApplySettings(9, 9, false);
             
             //Then
-           // Assert.AreEqual(viewModel.GetMouseHorizontal, 9f, "Mouse Horizontal value should be overriden");
-           // Assert.AreEqual(viewModel.GetMouseVertical, 9f, "Mouse Vertical value should be overriden");
-           // Assert.AreEqual(viewModel.GetControllerHorizontal, 9f, "Controller Horizontal value should be overriden");
-          //  Assert.AreEqual(viewModel.GetControllerVeritcal, 9f, "Controller Vertical value should be overriden");
+            Assert.AreEqual(viewModel.GetHorizontal, 9f, "Mouse Horizontal value should be overriden");
+            Assert.AreEqual(viewModel.GetVertical, 9f, "Mouse Vertical value should be overriden");
+            Assert.IsFalse(viewModel.GetIsInverted, "Input should not be inverted");
+        }
+        
+        [Test]
+        public void Test_ControllerApplySettings()
+        {
+            //Given
+            var inputSettings = new MockInputSettings
+            {
+                MouseHorizontalSensitivity = 0.1f,
+                MouseVerticalSensitivity = 0.2f,
+                ControllerHorizontalSensitivity = 0.3f,
+                ControllerVerticalSensitivity = 0.4f,
+                IsInverted = true
+            };
+            var viewModel = new ControllerInputSettingsViewModel(inputSettings, new MockAnalytics());
+
+            //When
+            viewModel.ApplySettings(9, 9, false);
+            
+            //Then
+            Assert.AreEqual(viewModel.GetHorizontal, 9f, "Controller Horizontal value should be overriden");
+            Assert.AreEqual(viewModel.GetVertical, 9f, "Controller Vertical value should be overriden");
             Assert.IsFalse(viewModel.GetIsInverted, "Input should not be inverted");
         }
     }
