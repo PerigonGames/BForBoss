fd256e65be3344aa0867316c4bb425e1431d9ba5
Martin Tsang <m4rtin.t@gmail.com>
2021-12-17 16:26:16 -0800

    BFB-143: Refactor Input Settings Menu (#86)
    **JIRA Ticket**
    https://perigongames.atlassian.net/browse/BFB-143
    
    **Description**
    - Refactor the Input settings to split between controller and mkb
    
    **Screenshots / GIFs**
    ![image](https://user-images.githubusercontent.com/5779541/146512525-c3cd26e4-6c3f-4193-b29e-e2e5cd6d02bf.png)

--- Assets/_BForBoss/_Core/Scripts/UserInterface/InputSettingsViewBehaviour.cs
+++ Assets/_BForBoss/_Core/Scripts/UserInterface/InputSettingsViewBehaviour.cs
@@ -1,6 +1,4 @@
-using Perigon.Analytics;
 using Perigon.UserInterface;
-using Perigon.Utility;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,20 +7,18 @@ namespace BForBoss
 {
     public class InputSettingsViewBehaviour : MonoBehaviour
     {
-        [Title("Settings")]
-        [SerializeField] private SliderBehaviour _mouseHorizontalSlider = null;
-        [SerializeField] private SliderBehaviour _mouseVerticalSlider = null;
-        [SerializeField] private SliderBehaviour _controllerHorizontalSlider = null;
-        [SerializeField] private SliderBehaviour _controllerVerticalSlider = null;
-        [SerializeField] private Toggle _toggle = null;
-
-        [Title("Button")] 
-        [SerializeField] private Button _applyButton = null;
-        [SerializeField] private Button _revertButton = null;
-
-        private InputSettingsViewModel _viewModel = null;
+        private BaseInputSettingsViewModel _viewModel = null;
         
-        public void Initialize(InputSettingsViewModel viewModel)
+        [Title("Settings")]
+        [SerializeField] protected SliderBehaviour _horizontalSlider = null;
+        [SerializeField] protected SliderBehaviour _verticalSlider = null;
+        [SerializeField] protected Toggle _toggle = null;
+        
+        [Title("Button")] 
+        [SerializeField] protected Button _applyButton = null;
+        [SerializeField] protected Button _revertButton = null;
+        
+        public void Initialize(BaseInputSettingsViewModel viewModel)
         {
             _viewModel = viewModel;
             SetupSliders();
@@ -37,49 +33,35 @@ namespace BForBoss
             _applyButton.onClick.AddListener(() =>
             {
                 _viewModel.ApplySettings(
-                    _mouseHorizontalSlider.SliderValue, 
-                    _mouseVerticalSlider.SliderValue,
-                    _controllerHorizontalSlider.SliderValue,
-                    _controllerVerticalSlider.SliderValue,
+                    _horizontalSlider.SliderValue, 
+                    _verticalSlider.SliderValue,
                     _toggle.isOn);
                 _applyButton.interactable = false;
             });
         }
-
+        
         private void BindSliders()
         {
-            _mouseHorizontalSlider.OnValueChangedAction = () =>
+            _horizontalSlider.OnValueChangedAction = () =>
             {
                 _applyButton.interactable = true;
             };
             
-            _mouseVerticalSlider.OnValueChangedAction = () =>
+            _verticalSlider.OnValueChangedAction = () =>
             {
                 _applyButton.interactable = true;
             };
-            
-            _controllerHorizontalSlider.OnValueChangedAction = () =>
-            {
-                _applyButton.interactable = true;
-            };
-            
-            _controllerVerticalSlider.OnValueChangedAction = () =>
-            {
-                _applyButton.interactable = true;
-            };
-            
+
             _toggle.onValueChanged.AddListener((_) =>
             {
                 _applyButton.interactable = true;
             });
         }
-
+        
         private void SetupSliders()
         {
-            _mouseHorizontalSlider.SliderValue = _viewModel.GetMouseHorizontal;
-            _mouseVerticalSlider.SliderValue = _viewModel.GetMouseVertical;
-            _controllerHorizontalSlider.SliderValue = _viewModel.GetControllerHorizontal;
-            _controllerVerticalSlider.SliderValue = _viewModel.GetControllerVeritcal;
+            _horizontalSlider.SliderValue = _viewModel.GetHorizontal;
+            _verticalSlider.SliderValue = _viewModel.GetVertical;
             _toggle.isOn = _viewModel.GetIsInverted;
             _applyButton.interactable = false;
         }
---
+++ Assets/_BForBoss/_Core/Scripts/UserInterface/MouseKeyboardInputSettingsViewModel.cs
@@ -0,0 +1,23 @@
+using Perigon.Analytics;
+using Perigon.Utility;
+
+namespace BForBoss
+{
+    public class MouseKeyboardInputSettingsViewModel: BaseInputSettingsViewModel
+    {
+        public override float GetHorizontal => _settings.MouseHorizontalSensitivity;
+        public override float GetVertical => _settings.MouseVerticalSensitivity;
+        
+        public MouseKeyboardInputSettingsViewModel(IInputSettings settings, IPerigonAnalytics analytics = null) : base(settings, analytics)
+        {
+        }
+
+        public override void ApplySettings(float horizontal, float vertical, bool isInverted)
+        {
+            _settings.MouseHorizontalSensitivity = horizontal;
+            _settings.MouseVerticalSensitivity = vertical;
+            _settings.IsInverted = isInverted;
+            SetInputSettingsAnalytics();
+        }
+    }
+}
