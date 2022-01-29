44c5575671208fd3d11086e3af4c137537c9dae0
Gursaanj <gursaanjresides@gmail.com>
2022-01-28 21:50:37 -0800

    IHealth Explicit Interface

--- Assets/_BForBoss/_Entities/Scripts/ScriptableObjects/HealthScriptableObject.cs
+++ Assets/_BForBoss/_Entities/Scripts/ScriptableObjects/HealthScriptableObject.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Perigon.Entities
@@ -15,6 +13,6 @@ namespace Perigon.Entities
     {
         [SerializeField] private float _health = 100f;
 
-        public float Health => _health;
+        float IHealth.Health => _health;
     }
 }
