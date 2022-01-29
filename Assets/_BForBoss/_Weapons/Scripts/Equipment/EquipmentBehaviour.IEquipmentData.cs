6c9be7c0a5f6b0d69d81dd94446e3eca5efb9493
Gursaanj <gursaanjresides@gmail.com>
2022-01-28 22:12:21 -0800

    IEquipmentData Explicit Interface

--- Assets/_BForBoss/_Weapons/Scripts/Equipment/EquipmentBehaviour.IEquipmentData.cs
+++ Assets/_BForBoss/_Weapons/Scripts/Equipment/EquipmentBehaviour.IEquipmentData.cs
@@ -11,12 +11,11 @@ namespace Perigon.Weapons
 
     public partial class EquipmentBehaviour: IEquipmentData
     {
-        public int AmmunitionAmount => CurrentWeapon.AmmunitionAmount;
-        public int MaxAmmunitionAmount => CurrentWeapon.MaxAmmunitionAmount;
-        public string NameOfWeapon => CurrentWeapon.NameOfWeapon;
-
-        public float MaxReloadDuration => CurrentWeapon.MaxReloadDuration;
-        public float ElapsedReloadDuration => CurrentWeapon.ElapsedReloadDuration;
+        int IEquipmentData.AmmunitionAmount => CurrentWeapon.AmmunitionAmount;
+        int IEquipmentData.MaxAmmunitionAmount => CurrentWeapon.MaxAmmunitionAmount;
+        string IEquipmentData.NameOfWeapon => CurrentWeapon.NameOfWeapon;
+        float IEquipmentData.MaxReloadDuration => CurrentWeapon.MaxReloadDuration;
+        float IEquipmentData.ElapsedReloadDuration => CurrentWeapon.ElapsedReloadDuration;
 
         public Weapon[] Weapons => _weapons;
         public Weapon CurrentWeapon => _weapons[_currentWeaponIndex];
