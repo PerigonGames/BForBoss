ffca7d06c1968b0df2a94996a0433bf2c7e3bf92
Gursaanj <gursaanjresides@gmail.com>
2022-02-08 19:23:46 -0800

    Relocated SceneSwitcher scripts

--- Assets/_BForBoss/_Utility/Scripts/Editor/SceneSwitcher/SceneSwitcherEditorWindow.cs
+++ Assets/_BForBoss/_Utility/Scripts/Editor/SceneSwitcher/SceneSwitcherEditorWindow.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
-using PerigonGames;
+using Sirenix.Utilities;
 using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
 
