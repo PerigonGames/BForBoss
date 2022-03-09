e6854f1796785202df0745c8eca0926f0d3d0915
Gursaanj <gursaanjresides@gmail.com>
2022-03-08 19:20:24 -0800

    Readd Initialization

--- Assets/_BForBoss/_Core/Scripts/Managers/BaseWorldManager.cs
+++ Assets/_BForBoss/_Core/Scripts/Managers/BaseWorldManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using Perigon.Character;
 using Perigon.Utility;
 using Sirenix.OdinInspector;
@@ -67,6 +69,31 @@ namespace BForBoss
                 Debug.LogError("PauseMenu is missing from World Manager");
             }
         }
+
+#if UNITY_EDITOR
+        private void OnGUI()
+        {
+            Event evt = Event.current;
+            if (evt.isKey && evt.keyCode == LevelDesignFeedbackWindowListener.WindowKeyCode)
+            {
+                StartCoroutine(OpenLevelDesignFeedbackWindow());
+            }
+        }
+
+        private IEnumerator OpenLevelDesignFeedbackWindow()
+        {
+            yield return new WaitForEndOfFrame();
+            Texture2D rawScreenShot = ScreenCapture.CaptureScreenshotAsTexture();
+            int width = rawScreenShot.width;
+            int height = rawScreenShot.height;
+            
+            Texture2D filteredScreenShot = new Texture2D(width, height, TextureFormat.ARGB32, false);
+            filteredScreenShot.ReadPixels(new Rect(0,0,width, height), 0 ,0);
+            filteredScreenShot.Apply();
+
+            LevelDesignFeedbackWindowListener.OpenLevelDesignFeedbackWindow(filteredScreenShot);
+        }
+#endif
 
         private void HandleStateChange(State newState)
         {
