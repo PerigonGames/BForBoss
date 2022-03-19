a439402d616d2d7197ce95aa1ba4e1077abe7077
Gursaanj <gursaanjresides@gmail.com>
2022-03-19 16:47:28 -0700

    Changed Namespaces

--- Assets/_BForBoss/_Core/Scripts/Managers/BaseWorldManager.cs
+++ Assets/_BForBoss/_Core/Scripts/Managers/BaseWorldManager.cs
@@ -70,30 +70,30 @@ namespace BForBoss
             }
         }
 
-#if UNITY_EDITOR
-        private void OnGUI()
-        {
-            Event evt = Event.current;
-            if (evt.isKey && evt.keyCode == LevelDesignFeedbackWindowListener.WindowKeyCode)
-            {
-                StartCoroutine(OpenLevelDesignFeedbackWindow());
-            }
-        }
-
-        private IEnumerator OpenLevelDesignFeedbackWindow()
-        {
-            yield return new WaitForEndOfFrame();
-            Texture2D rawScreenShot = ScreenCapture.CaptureScreenshotAsTexture();
-            int width = rawScreenShot.width;
-            int height = rawScreenShot.height;
-            
-            Texture2D filteredScreenShot = new Texture2D(width, height, TextureFormat.ARGB32, false);
-            filteredScreenShot.ReadPixels(new Rect(0,0,width, height), 0 ,0);
-            filteredScreenShot.Apply();
-
-            LevelDesignFeedbackWindowListener.OpenLevelDesignFeedbackWindow(filteredScreenShot);
-        }
-#endif
+// #if UNITY_EDITOR
+//         private void OnGUI()
+//         {
+//             Event evt = Event.current;
+//             if (evt.isKey && evt.keyCode == LevelDesignFeedbackWindowListener.WindowKeyCode)
+//             {
+//                 StartCoroutine(OpenLevelDesignFeedbackWindow());
+//             }
+//         }
+//
+//         private IEnumerator OpenLevelDesignFeedbackWindow()
+//         {
+//             yield return new WaitForEndOfFrame();
+//             Texture2D rawScreenShot = ScreenCapture.CaptureScreenshotAsTexture();
+//             int width = rawScreenShot.width;
+//             int height = rawScreenShot.height;
+//             
+//             Texture2D filteredScreenShot = new Texture2D(width, height, TextureFormat.ARGB32, false);
+//             filteredScreenShot.ReadPixels(new Rect(0,0,width, height), 0 ,0);
+//             filteredScreenShot.Apply();
+//
+//             LevelDesignFeedbackWindowListener.OpenLevelDesignFeedbackWindow(filteredScreenShot);
+//         }
+// #endif
 
         private void HandleStateChange(State newState)
         {
---
+++ Assets/_BForBoss/_Utility/Scripts/Editor/LevelDesignFeedback/LevelDesignFeedbackWindowListener.cs
@@ -0,0 +1,36 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Perigon.Utility
+{
+    public static class LevelDesignFeedbackWindowListener
+    {
+        public static KeyCode WindowKeyCode = KeyCode.F10;
+        private static LevelDesignFeedbackEditorWindow _window = null;
+
+        private static bool _isApplicationPaused = false;
+
+        public static void OpenLevelDesignFeedbackWindow(Texture2D screenShot)
+        {
+            if (!CanOpenFeedbackWindow() || _window != null)
+            {
+                return;
+            }
+
+            _window = LevelDesignFeedbackEditorWindow.OpenWindow(screenShot);
+            _window.OnWindowClosed = OnWindowClosed;
+        }
+
+        private static void OnWindowClosed()
+        {
+            _window = null;
+            EditorApplication.isPaused = _isApplicationPaused;
+        }
+
+        private static bool CanOpenFeedbackWindow()
+        {
+            _isApplicationPaused = EditorApplication.isPaused;
+            return (EditorApplication.isPlaying || _isApplicationPaused) && Application.isFocused;
+        }
+    }
+}
