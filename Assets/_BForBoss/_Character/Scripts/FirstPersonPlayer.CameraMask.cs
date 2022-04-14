f7febb3a210955a2bc54e4b06c766174f736d279
Martin <m4rtin.t@gmail.com>
2022-04-13 19:06:33 -0700

    Setup Layer Mask

---
+++ Assets/_BForBoss/_Character/Scripts/FirstPersonPlayer.CameraMask.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Perigon.Character
+{
+    public partial class FirstPersonPlayer
+    {
+        private const string PLAYER_MODEL_LAYER = "PlayerModel";
+        private const string FIRST_PERSON_WEAPON_LAYER = "FirstPersonWeapon";
+        
+        private int FirstPersonMask => ~(1 << LayerMask.NameToLayer(PLAYER_MODEL_LAYER) | 1 << LayerMask.NameToLayer(FIRST_PERSON_WEAPON_LAYER));
+
+        private void SetCameraCullingMask()
+        {
+            camera.cullingMask = FirstPersonMask;
+        }
+    }
+}
