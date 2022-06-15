62bf85c3b9385b6ddc7010c3153700e442633bc3
Gursaanj Singh <gursaanjresides@gmail.com>
2022-06-14 20:51:16 -0700

    Gursaanj/BFB-211: Debug Free Roam Camera (#185)
    * Initial Camera Setup
    
    * Working FreeRoam Camera
    
    * Remove Non Input System related code from FreeRoam Camera
    
    * Feedback changes
    
    * Working FreeRoam Camera with SceneSwitcher
    
    * Merge with Develop
    
    * Update AdditiveDebugScene.unity
    
    * Revert "Merge with Develop"
    
    This reverts commit 8f3b3520640f2fc824a677334086f327667436a2.
    
    * Label Typo fix
    
    * Place FreeRoamCamera into Utility instead of Debug
    
    * Fix Pause on DebugMenu Glitch

---
+++ Assets/_BForBoss/_Core/Scripts/Debug/DebugView/FreeCamera.cs
@@ -0,0 +1,64 @@
+using System;
+using UnityEngine;
+
+namespace BForBoss
+{
+    public class FreeCamera : DebugView
+    {
+        private GUIStyle _boldInstructionStyle = null;
+        private Action _onBackButtonPressed = null;
+        
+        public FreeCamera(Rect masterRect, Action onWindowOpened, Action onBackButtonPressed) : base(masterRect)
+        {
+            _onBackButtonPressed = onBackButtonPressed;
+            onWindowOpened?.Invoke();
+        }
+
+        public override void ResetData()
+        {
+            base.ResetData();
+            _onBackButtonPressed?.Invoke();
+        }
+
+        protected override void DrawWindow()
+        {
+            if (_boldInstructionStyle == null)
+            {
+                _boldInstructionStyle = new GUIStyle(GUI.skin.label)
+                {
+                    normal = new GUIStyleState
+                    {
+                        textColor = Color.white
+                    },
+                    fontStyle = FontStyle.BoldAndItalic,
+                    alignment = TextAnchor.MiddleCenter
+                };
+            }
+            
+            using (new GUILayout.VerticalScope())
+            {
+                GUILayout.Label("");
+                DrawInstruction("Use", "WASD", "to move");
+                DrawInstruction("Use", "Q", "to pan down");
+                DrawInstruction("Use", "E", "to pan up");
+                DrawInstruction("Hold", "Left Shift", "to boost");
+                DrawInstruction("Use", "Scroll Wheel", "to modify boost multiplier");
+                DrawInstruction("Hold", "Right mouse", "to rotate");
+                DrawInstruction("Press", "SpaceBar", "to reset");
+                DrawInstruction("Press", "Backquote", "to escape");
+            }
+            
+            GUI.UnfocusWindow();
+        }
+
+        private void DrawInstruction(string prefix, string instruction, string suffix)
+        {
+            using (new GUILayout.HorizontalScope())
+            {
+                GUILayout.Label($"{prefix} ", GUILayout.ExpandWidth(false));
+                GUILayout.Label($"{instruction} ", _boldInstructionStyle,GUILayout.ExpandWidth(false));
+                GUILayout.Label(suffix);
+            }
+        }
+    }
+}
