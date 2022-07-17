7543d411326d0bb5a36cf702e597cf89af6af57d
Gursaanj Singh <gursaanjresides@gmail.com>
2022-07-17 00:22:10 -0700

    BFB-355 : Added Enemy Spawner (#207)
    * Base Scene Made
    
    * Working Scene

---
+++ Assets/_BForBoss/_Core/Scripts/Managers/EnemySpawnerManager.cs
@@ -0,0 +1,41 @@
+using System;
+using Perigon.Entities;
+using UnityEngine;
+
+namespace BForBoss
+{
+    public class EnemySpawnerManager : MonoBehaviour
+    {
+        private EnemySpawner[] _enemySpawners = null;
+        public void Reset()
+        {
+            if (_enemySpawners == null)
+            {
+                return;
+            }
+
+            foreach (EnemySpawner spawner in _enemySpawners)
+            {
+                spawner.Reset();
+            }
+        }
+
+        public void Initialize(LifeCycleManager lifeCycleManager)
+        {
+            if (_enemySpawners == null)
+            {
+                return;
+            }
+
+            foreach (EnemySpawner spawner in _enemySpawners)
+            {
+                spawner.Initialize(lifeCycleManager);
+            }
+        }
+
+        private void Awake()
+        {
+            _enemySpawners = FindObjectsOfType<EnemySpawner>();
+        }
+    }
+}
