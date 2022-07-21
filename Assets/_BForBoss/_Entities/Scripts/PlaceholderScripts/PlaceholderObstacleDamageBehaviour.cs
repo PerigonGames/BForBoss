a7b08c14eacc8929e3d8d2f84ce5384d6012626b
Martin Tsang <m4rtin.t@gmail.com>
2022-07-20 22:26:30 -0700

    BFB-365: AI Shooting (#208)
    * Remove enemy navigation
    
    * Added Shooter
    
    * Update enemy behaviour to follow after shooting
    
    * Update enemy settings
    
    * Added health pill and fixed editor build scenes
    
    * Update MeleeWeaponBehaviour.cs
    
    * PR comment updates
    
    * Update EnemyShootingBehaviour.cs
    
    * Locally reset ShootingBehaviour
    
    * Updated naming for interface

--- Assets/_BForBoss/_Entities/Scripts/PlaceholderScripts/PlaceholderObstacleDamageBehaviour.cs
+++ Assets/_BForBoss/_Entities/Scripts/PlaceholderScripts/PlaceholderObstacleDamageBehaviour.cs
@@ -10,7 +10,7 @@ namespace Perigon.Entities
             var lifeCycle = other.gameObject.GetComponent<LifeCycleBehaviour>();
             if (lifeCycle != null)
             {
-                lifeCycle.Damage(_damage);
+                lifeCycle.DamagedBy(_damage);
             }
         }
     }
