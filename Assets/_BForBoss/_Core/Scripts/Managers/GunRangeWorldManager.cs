c5f153fbc54a1cb9a5523d275da7e6b16c510a10
AbelW <unoctium1@gmail.com>
2022-08-02 20:06:08 -0700

    Update extant world managers

--- Assets/_BForBoss/_Core/Scripts/Managers/GunRangeWorldManager.cs
+++ Assets/_BForBoss/_Core/Scripts/Managers/GunRangeWorldManager.cs
@@ -1,41 +1,17 @@
 using System;
-using Perigon.Entities;
 using Perigon.Utility;
-using Perigon.Weapons;
 using Sirenix.OdinInspector;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace BForBoss
 {
     public class GunRangeWorldManager : BaseWorldManager
     {
-        [Title("Component")] 
-        [SerializeField] private WeaponAnimationController _weaponAnimationController = null;
-        [SerializeField] private EquipmentBehaviour _equipmentBehaviour = null;
-        [SerializeField] private AmmunitionCountViewBehaviour _ammunitionCountView = null;
-        [SerializeField] private ReloadViewBehaviour _reloadView = null;
-        [SerializeField] private MeleeViewBehaviour _meleeView = null;
+        [Title("Component")]
         [SerializeField] private LifeCycleManager _lifeCycleManager = null;
 
         protected override Vector3 SpawnLocation => Vector3.zero;
         protected override Quaternion SpawnLookDirection => Quaternion.identity;
-
-        protected override void Start()
-        {
-            base.Start();
-            _weaponAnimationController.Initialize(
-                () => _playerBehaviour.PlayerMovement.CharacterVelocity,
-                () => _playerBehaviour.PlayerMovement.CharacterMaxSpeed,
-                () => _playerBehaviour.PlayerMovement.IsWallRunning,
-                () => _playerBehaviour.PlayerMovement.IsGrounded,
-                () => _playerBehaviour.PlayerMovement.IsSliding,
-                () => _playerBehaviour.PlayerMovement.IsDashing);
-            _equipmentBehaviour.Initialize(_playerBehaviour.PlayerMovement.RootPivot);
-            _ammunitionCountView.Initialize(_equipmentBehaviour);
-            _reloadView.Initialize(_equipmentBehaviour);
-            _meleeView.Initialize(_equipmentBehaviour);
-        }
 
         protected override void Reset()
         {
@@ -46,31 +22,7 @@ namespace BForBoss
         protected override void OnValidate()
         {
             base.OnValidate();
-            if (_weaponAnimationController == null)
-            {
-                PanicHelper.Panic(new Exception("Weapons animation controller missing from World Manager"));
-            }
-            
-            if (_equipmentBehaviour == null)
-            {
-                PanicHelper.Panic(new Exception("Equipment Behaviour missing from World Manager"));
-            }
-            
-            if (_ammunitionCountView == null)
-            {
-                PanicHelper.Panic(new Exception("Ammunition Count View missing from World Manager"));
-            }
-            
-            if (_reloadView == null)
-            {
-                PanicHelper.Panic(new Exception("Reload View missing from World Manager"));
-            }
-            
-            if (_meleeView == null)
-            {
-                PanicHelper.Panic(new Exception("Melee View missing from World Manager"));
-            }
-            
+
             if (_lifeCycleManager == null)
             {
                 PanicHelper.Panic(new Exception("Life Cycle Manager missing from World Manager"));
