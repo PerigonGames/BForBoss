8652632ef2a23c86ec12ef6ae3d932f262366f65
AbelW <unoctium1@gmail.com>
2022-08-06 21:14:04 -0700

    Add support for proper knockback to flying enemies. Currently causes them to spin out, sort of unintended but I think its kinda cool

--- Assets/_BForBoss/_Core/Scripts/Enemies/FloatingTargetBehaviour.cs
+++ Assets/_BForBoss/_Core/Scripts/Enemies/FloatingTargetBehaviour.cs
@@ -6,18 +6,28 @@ using UnityEngine;
 namespace BForBoss
 {
     [RequireComponent(typeof(AgentNavigationBehaviour))]
-    public class FloatingTargetBehaviour : EnemyBehaviour
+    public class FloatingTargetBehaviour : EnemyBehaviour, IKnockback
     {
         [SerializeField] private HealthbarViewBehaviour _healthbar;
+
+        [Header("Knockback Settings")]
+        [SerializeField] private float _knockbackRadiusModifier = 10f;
+        [SerializeField] private float _knockbackUpwardsForceModifier = 10f;
+        [SerializeField] private float _knockbackMaxDuration = 2f;
+        
         private AgentNavigationBehaviour _navigationBehaviour = null;
         private EnemyShootingBehaviour _shootingBehaviour = null;
         private FloatingEnemyAnimationBehaviour _animationBehaviour = null;
         private FloatingTargetState _state = FloatingTargetState.MoveTowardsDestination;
+        
+        private Rigidbody _rb;
+        private float _knockbackCurrentTime;
 
         private enum FloatingTargetState
         {
             MoveTowardsDestination,
-            ShootTarget
+            ShootTarget,
+            KnockedBack
         }
         
         public void Initialize(Func<Vector3> getPlayerPosition, BulletSpawner bulletSpawner, Action onDeathCallback, Action<EnemyBehaviour> onReleaseToSpawner = null)
@@ -63,14 +73,48 @@ namespace BForBoss
             {
                 gameObject.SetActive(false);
             }
+            ToggleKnockbackState(false);
         }
 
         private void Awake()
         {
+            _rb = GetComponent<Rigidbody>();
+            if (_rb != null)
+            {
+                _rb.isKinematic = true;
+                _rb.useGravity = false;
+            }
             if (_healthbar == null)
             {                
                 Debug.LogWarning("A FloatingTargetBehaviour is missing a health bar");
             }
+        }
+
+        private void CheckKnockback(float time)
+        {
+            if (_rb == null) 
+                return;
+            _knockbackCurrentTime += time;
+            if (_knockbackCurrentTime > _knockbackMaxDuration || _rb.velocity.sqrMagnitude < float.Epsilon * float.Epsilon)
+            {
+                ToggleKnockbackState(false);
+            }
+        }
+
+        private void ToggleKnockbackState(bool active)
+        {
+            if (_rb == null)
+                return;
+            if (active)
+            {
+                _knockbackCurrentTime = 0f;
+                _navigationBehaviour.PauseNavigation();
+            }
+            else
+                _navigationBehaviour.ResumeNavigation();
+            _rb.isKinematic = !active;
+            _rb.useGravity = active;
+            _state = active ? FloatingTargetState.KnockedBack : FloatingTargetState.MoveTowardsDestination;
         }
         
         private void FixedUpdate()
@@ -83,9 +127,19 @@ namespace BForBoss
                 case FloatingTargetState.ShootTarget:
                     _shootingBehaviour.ShootingUpdate();
                     break;
+                case FloatingTargetState.KnockedBack:
+                    CheckKnockback(Time.fixedDeltaTime);
+                    break;
             }
         }
-        
 
+
+        public void ApplyKnockback(float force, Vector3 originPosition)
+        {
+            if (_rb == null || !_lifeCycle.IsAlive)
+                return;
+            ToggleKnockbackState(true);
+            _rb.AddExplosionForce(force, originPosition, _knockbackRadiusModifier, _knockbackUpwardsForceModifier);
+        }
     }
 }
