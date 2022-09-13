ca873197bd0c421359107fcb99aa38fae9efd502
Martin Tsang <m4rtin.t@gmail.com>
2022-09-13 06:37:32 -0700

    BFB-416: Shooting Animation Timing + Enemy can damage player (#260)
    * Update EnemyShootingBehaviour.cs
    
    * Other bug fixes

--- Assets/_BForBoss/_Core/Scripts/Enemies/EnemyShootingBehaviour.cs
+++ Assets/_BForBoss/_Core/Scripts/Enemies/EnemyShootingBehaviour.cs
@@ -104,7 +104,6 @@ namespace BForBoss
 
         private void CountDownUntilShoot()
         {
-            _enemyAnimation.SetShootingAnimation();
             _elapsedShootCountDown -= Time.deltaTime;
             if (_elapsedShootCountDown <= 0)
             {
@@ -131,6 +130,7 @@ namespace BForBoss
         private void Shoot()
         {
             _muzzleFlashVFX.SendEvent(_vfxFireEvent);
+            _enemyAnimation.SetShootingAnimation();
             var bullet = _bulletSpawner.SpawnBullet();
             bullet.SetSpawnAndDirection(_shootingFromPosition.position, _shootDirection.normalized);
         }
