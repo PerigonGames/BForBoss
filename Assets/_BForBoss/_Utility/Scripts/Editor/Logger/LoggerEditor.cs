16fcd69fa06a6d805f6aef66945a1a3f75ec7d96
AbelW <unoctium1@gmail.com>
2022-09-17 19:16:57 -0700

    Clean up usings

--- Assets/_BForBoss/_Utility/Scripts/Editor/Logger/LoggerEditor.cs
+++ Assets/_BForBoss/_Utility/Scripts/Editor/Logger/LoggerEditor.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using UnityEditor;
-using UnityEditor.Compilation;
 using UnityEngine;
 
 namespace Perigon.Utility.Editor
