0939a670329317b3b951619ac2c74d58b4e0a0bc
Martin Tsang <m4rtin.t@gmail.com>
2022-10-17 00:01:47 -0700

    BFB-463: Dev only Timer UI (#294)
    * Dev only Timer
    
    * Update WaveSandboxWorldManager.cs

--- Assets/_BForBoss/_Core/Scripts/Managers/WeaponSandboxWorldManager.cs
+++ Assets/_BForBoss/_Core/Scripts/Managers/WeaponSandboxWorldManager.cs
@@ -4,11 +4,7 @@ namespace BForBoss
 {
     public class WeaponSandboxWorldManager : BaseWorldManager
     {
-        [SerializeField]
-        private Transform _spawnLocation = null;
-
         protected override Vector3 SpawnLocation => _spawnLocation.position;
         protected override Quaternion SpawnLookDirection => _spawnLocation.rotation;
-
     }
 }
