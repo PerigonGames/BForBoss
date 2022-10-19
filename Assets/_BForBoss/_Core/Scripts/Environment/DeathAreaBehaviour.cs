68a8171d6437ed56d36c1afca6c946b9cda8e70f
Cole Sweet <94721502+csweet22@users.noreply.github.com>
2022-10-19 10:22:10 -0400

    BFB-459: Added Death Plane to Wave Map (#296)
    * Added in Deathplane. Changed Deathplane code.
    
    DeathAreaBehaviour effectively works as a reset behaviour now instead.
    
    * Added death plane mat
    
    Co-authored-by: Martin Tsang <m4rtin.t@gmail.com>

--- Assets/_BForBoss/_Core/Scripts/Environment/DeathAreaBehaviour.cs
+++ Assets/_BForBoss/_Core/Scripts/Environment/DeathAreaBehaviour.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 namespace BForBoss
 {
     public class DeathAreaBehaviour : MonoBehaviour
-    {        
+    {
+        [SerializeField] private string _deathAreaName = "deathArea";
+        
         private void OnCollisionEnter(Collision other)
         {
-            StateManager.Instance.SetState(State.Death);
+            StateManager.Instance.SetState(State.PreGame);
         }
     }
 }
