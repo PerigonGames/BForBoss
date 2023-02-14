160f0d22d42a1291ff766e87b24fb2397f58d193
Martin Tsang <m4rtin.t@gmail.com>
2023-02-13 23:32:37 -0500

    Update WallRunSO.cs

--- Assets/_BForBoss/_Core/Scripts/Environment/WallRunning/WallRunSO.cs
+++ Assets/_BForBoss/_Core/Scripts/Environment/WallRunning/WallRunSO.cs
@@ -41,11 +41,11 @@ namespace BForBoss
         private float _cameraRotateDuration = 1f;
         [FoldoutGroup("Camera Settings")]
         [SerializeField] 
-        [InfoBox("How fast the camera rotates towards where the player is wall running along")]
+        [Tooltip("How fast the camera rotates towards where the player is wall running along")]
         private float _lookAlongWallRotationSpeed = 3f;
         [FoldoutGroup("Camera Settings")]
         [SerializeField]
-        [InfoBox("The angle between where you're looking at and the direction where you're wall running towards")]
+        [Tooltip("The angle between where you're looking at and the direction where you're wall running towards")]
         private float _minLookAlongWallStabilizationAngle = 5f;
 
         public WallRunData MapToData()
