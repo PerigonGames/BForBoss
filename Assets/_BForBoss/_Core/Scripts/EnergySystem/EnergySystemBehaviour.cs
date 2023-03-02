a5bcc4556cd4a5ee47c35ef56010870aa274f12e
Martin Tsang <m4rtin.t@gmail.com>
2023-03-01 18:07:23 -0800

    Energy System (#322)

---
+++ Assets/_BForBoss/_Core/Scripts/EnergySystem/EnergySystemBehaviour.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Collections.Generic;
+using Perigon.Utility;
+using UnityEngine;
+using Logger = Perigon.Utility.Logger;
+
+namespace BForBoss
+{
+    public interface IEnergySystem
+    {
+        void Accrue(EnergyAccruementType accruementType, float multiplier = 1);
+        void Expend(EnergyExpenseType expenseType, float multiplier = 1);
+    }
+    
+    public class EnergySystemBehaviour : MonoBehaviour, IEnergySystem
+    {
+        [SerializeField] private EnergySystemConfigurationSO _energySystemConfiguration;
+        [SerializeField] private EnergySO _energy;
+
+        private EnergyData _energyData;
+        public EnergyData EnergyData
+        {
+            private set
+            {
+                _energyData = value;
+                Logger.LogFormat($"Energy Value: {_energyData.Value}", key: "energysystem");  
+            } 
+
+            get => _energyData;
+        }
+        
+        private EnergySystemConfigurationData _energySystemConfigurationData;
+
+        private readonly Queue<(EnergyAccruementType, float)> _accruedEnergyTypeQueue = new();
+        private readonly Queue<(EnergyExpenseType, float)> _expendEnergyTypeQueue = new();
+
+        private float _elapsedTime = 0;
+
+        public void Accrue(EnergyAccruementType accruementType, float multiplier = 1)
+        {
+            switch (accruementType)
+            {
+                case EnergyAccruementType.WallRun:
+                    _accruedEnergyTypeQueue.Enqueue((accruementType, multiplier));
+                    break;
+                default:
+                    AccrueEnergyImmediately(accruementType, multiplier);
+                    break;
+            }
+        }
+
+        public void Expend(EnergyExpenseType expenseType, float multiplier = 1)
+        {
+            switch (expenseType)
+            {
+                case EnergyExpenseType.SlowMo:
+                    _expendEnergyTypeQueue.Enqueue((expenseType, multiplier));
+                    break;
+                default:
+                    ExpendEnergyImmediately(expenseType, multiplier);
+                    break;
+            }
+            
+        }
+
+        private void Awake()
+        {
+            if (_energySystemConfiguration == null)
+            {
+                PanicHelper.Panic(new Exception("Energy system configuration scriptable object missing from EnergySystemBehaviour"));
+            }
+
+            if (_energy == null)
+            {
+                PanicHelper.Panic(new Exception("Energy scriptable object missing from EnergySystemBehaviour"));
+            }
+        }
+
+        private void Start()
+        {
+            EnergyData = _energy.MapToData();
+            _energySystemConfigurationData = _energySystemConfiguration.MapToData();
+        }
+
+        private void Update()
+        {
+            _elapsedTime -= Time.deltaTime;
+            if (_elapsedTime < 0)
+            {
+                _elapsedTime = EnergyData.RateOfTransaction;
+                AccrueEnergyFromQueue();
+                ExpendEnergyFromQueue();
+            }
+
+            RemoveExtraEnergyFromQueue();
+        }
+        
+        private void AccrueEnergyFromQueue()
+        {
+            if (_accruedEnergyTypeQueue.TryPeek(out var result))
+            {
+                AccrueEnergyImmediately(result.Item1, result.Item2);
+            }
+        }
+        
+        private void ExpendEnergyFromQueue()
+        {
+            if (_expendEnergyTypeQueue.TryPeek(out var result))
+            {
+                ExpendEnergyImmediately(result.Item1, result.Item2);
+            }
+        }
+
+        private void AccrueEnergyImmediately(EnergyAccruementType type, float multiplier)
+        {
+            var energy = Math.Min(_energyData.Value + MapToAccrueValue(type, multiplier), _energyData.MaxEnergyValue);
+            EnergyData = _energyData.Apply(energy);
+        }
+        
+        private void ExpendEnergyImmediately(EnergyExpenseType type, float multiplier)
+        {
+            var energy = Math.Max(_energyData.Value - MapToExpendValue(type, multiplier), 0);
+            EnergyData = _energyData.Apply(energy);
+        }
+
+        private void RemoveExtraEnergyFromQueue()
+        {
+            if (_accruedEnergyTypeQueue.Count > 0)
+            {
+                _accruedEnergyTypeQueue.Dequeue();
+            }
+            
+            if (_expendEnergyTypeQueue.Count > 0)
+            {
+                _expendEnergyTypeQueue.Dequeue();
+            }
+        }
+
+        private float MapToAccrueValue(EnergyAccruementType type, float multiplier)
+        {
+            switch (type)
+            {
+                case EnergyAccruementType.WallRun:
+                    return _energySystemConfigurationData.WallRunEnergy * multiplier;
+                case EnergyAccruementType.Dash:
+                    return _energySystemConfigurationData.DashEnergy * multiplier;
+                case EnergyAccruementType.DoubleJump:
+                    return _energySystemConfigurationData.DoubleJumpEnergy * multiplier;
+                case EnergyAccruementType.Slide:
+                    return _energySystemConfigurationData.SlideEnergy * multiplier;
+                default:
+                    PanicHelper.Panic(new Exception("Missing Case for MapToAccrueValue"));
+                    return 0;
+            }
+        }
+        
+        private float MapToExpendValue(EnergyExpenseType type, float multiplier)
+        {
+            switch (type)
+            {
+                case EnergyExpenseType.Shot:
+                    return _energySystemConfigurationData.ShotEnergy * multiplier;
+                case EnergyExpenseType.SlowMo:
+                    return _energySystemConfigurationData.SlowMoEnergy * multiplier;
+                default:
+                    PanicHelper.Panic(new Exception("Missing Case for MapToExpendValue"));
+                    return 0;
+            }
+        }
+
+    }
+}
