16eb7003ac5ffdc91ae739fe7e19dbba56361fd0
AbelW <unoctium1@gmail.com>
2023-03-14 19:41:41 -0700

    Add improper usage test

--- Assets/_BForBoss/Tests/LaborSystemTests.cs
+++ Assets/_BForBoss/Tests/LaborSystemTests.cs
@@ -73,5 +73,31 @@ namespace Tests.Labors
             // Then
             Assert.IsTrue(finishedQueue);
         }
+        
+        [Test]
+        public void CompleteInactiveLaborTest()
+        {
+            // Given
+            var mock1 = new MockLabor();
+            var mock2 = new MockLabor();
+            var mock3 = new MockLabor();
+            var mock4 = new MockLabor();
+            var array = new ILabor[] {mock1, mock2, mock3, mock4};
+            
+            var system = new LaborSystem();
+            var finishedQueue = false;
+                
+            // When
+            system.onQueueCompleted += () => finishedQueue = true;
+            system.Initialize(array);
+            mock1.CompleteLabor();
+            mock3.CompleteLabor();
+            mock4.CompleteLabor();
+            
+            // Then
+            Assert.IsFalse(finishedQueue);
+            Assert.AreEqual(system.CurrentLabor, mock2);
+            Assert.IsFalse(mock3.IsActivated);
+        }
     }
 }
