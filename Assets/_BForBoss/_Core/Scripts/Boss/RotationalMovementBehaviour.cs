2a8dc358c77c4beaa457262f2385acee3f263f1c
Martin <m4rtin.t@gmail.com>
2023-03-22 20:42:48 -0700

    Rotational Script

---
+++ Assets/_BForBoss/_Core/Scripts/Boss/RotationalMovementBehaviour.cs
@@ -0,0 +1,46 @@
+using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Perigon.Entities
+{
+    public enum RotationState
+    {
+        ClockWise,
+        CounterClockWise
+    }
+    public class RotationalMovementBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private float rotationRate = 30f;
+        private int _direction = 0;
+
+        private void Update()
+        {
+            if (_direction != 0)
+            {
+                transform.Rotate(Vector3.up, _direction * rotationRate * Time.deltaTime);
+            }
+        }
+
+        [Button]
+        public void StartRotation(RotationState rotation)
+        {
+            switch (rotation)
+            {
+                case RotationState.ClockWise:
+                    _direction = 1;
+                    break;
+                case RotationState.CounterClockWise:
+                    _direction = -1;
+                    break;
+            }
+        }
+
+        [Button]
+        public void StopRotation()
+        {
+            _direction = 0;
+        }
+    }
+}
