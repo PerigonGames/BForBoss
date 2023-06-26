2dff419c7dc4e33c66a98cf545327a583bcee8ad
Gursaanj <gursaanjresides@gmail.com>
2023-06-26 03:31:20 -0700

    Revert SandboxWorldManager to original settings

--- Assets/_BForBoss/_Core/Scripts/Managers/SandboxWorldManager.cs
+++ Assets/_BForBoss/_Core/Scripts/Managers/SandboxWorldManager.cs
@@ -11,8 +11,8 @@ namespace BForBoss
 
         [SerializeField] private RingLaborManager _ringLaborManager;
         [SerializeField] private BossWipeOutWallsManager _wipeOutWallsManager;
-        [SerializeField] private DerekContextManager _derekContextManager;
-        //private DerekMissileLauncherBehaviour[] _derekMissileLauncherBehaviours;
+
+        private DerekMissileLauncherBehaviour[] _derekMissileLauncherBehaviours;
         private CountdownViewBehaviour _countdownTimer;
 
         protected override Vector3 SpawnLocation => _spawnLocation.position;
@@ -22,22 +22,20 @@ namespace BForBoss
         {
             base.Reset();
             _ringLaborManager.Reset();
-            _derekContextManager.Reset();
         }
 
         protected override void Start()
         {
             base.Start();
             _ringLaborManager.Initialize();
-            _derekContextManager.Initialize(_ringLaborManager, _playerBehaviour.PlayerMovement);
-            //_wipeOutWallsManager.Initialize(_playerBehaviour.PlayerMovement);
-            //_derekMissileLauncherBehaviours.ForEach(launcher => launcher.Initialize(_playerBehaviour.PlayerMovement));
+            _wipeOutWallsManager.Initialize(_playerBehaviour.PlayerMovement);
+            _derekMissileLauncherBehaviours.ForEach(launcher => launcher.Initialize(_playerBehaviour.PlayerMovement));
         }
 
         protected override void Awake()
         {
             base.Awake();
-           //_derekMissileLauncherBehaviours = FindObjectsOfType<DerekMissileLauncherBehaviour>();
+            _derekMissileLauncherBehaviours = FindObjectsOfType<DerekMissileLauncherBehaviour>();
         }
 
         protected override void OnValidate()
@@ -46,11 +44,6 @@ namespace BForBoss
             if (_ringLaborManager == null)
             {
                 PanicHelper.Panic(new Exception("_ringLaborManager is missing from Sandbox World Manager"));
-            }
-
-            if (_derekContextManager == null)
-            {
-                PanicHelper.Panic(new Exception($"{nameof(_derekContextManager)} is missing from Sandbox World Manager"));
             }
         }
     }
