d1c9305a0054ebdc98ff53f3386e45f94a3735f4
Gursaanj <gursaanjresides@gmail.com>
2023-06-26 03:23:52 -0700

    DerekWorldManager
    Created new World manager specific for DerekWorld, so current functionality in the Sandbox world manager does not break

---
+++ Assets/_BForBoss/_Core/Scripts/Managers/DerekWorldManager.cs
@@ -0,0 +1,45 @@
+using System;
+using BForBoss.RingSystem;
+using Perigon.Utility;
+using UnityEngine;
+
+namespace BForBoss
+{
+    public class DerekWorldManager : BaseWorldManager
+    {
+        [SerializeField] private RingLaborManager _ringLaborManager;
+        [SerializeField] private DerekContextManager _derekContextManager;
+        private CountdownViewBehaviour _countdownTimer;
+
+        protected override Vector3 SpawnLocation => _spawnLocation.position;
+        protected override Quaternion SpawnLookDirection => _spawnLocation.rotation;
+
+        protected override void Reset()
+        {
+            base.Reset();
+            _ringLaborManager.Reset();
+            _derekContextManager.Reset();
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+            _ringLaborManager.Initialize();
+            _derekContextManager.Initialize(_ringLaborManager, _playerBehaviour.PlayerMovement);
+        }
+
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+            if (_ringLaborManager == null)
+            {
+                PanicHelper.Panic(new Exception("_ringLaborManager is missing from Sandbox World Manager"));
+            }
+
+            if (_derekContextManager == null)
+            {
+                PanicHelper.Panic(new Exception($"{nameof(_derekContextManager)} is missing from Sandbox World Manager"));
+            }
+        }
+    }
+}
