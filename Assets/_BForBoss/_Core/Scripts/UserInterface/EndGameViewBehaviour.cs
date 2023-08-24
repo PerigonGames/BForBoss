5875529f7f280ed429521d95b666db8697f63c3a
Martin <m4rtin.t@gmail.com>
2023-08-23 19:36:06 -0700

    Victory screen

--- Assets/_BForBoss/_Core/Scripts/UserInterface/EndGameViewBehaviour.cs
+++ Assets/_BForBoss/_Core/Scripts/UserInterface/EndGameViewBehaviour.cs
@@ -1,4 +1,5 @@
 using Perigon.Utility;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,9 +7,13 @@ namespace BForBoss
 {
     public class EndGameViewBehaviour : MonoBehaviour
     {
+        private const string VictoryLabel = "Victory";
+        private const string GameOverLabel = "Game Over";
+            
         private readonly QuitGameUseCase _quitGameUseCase = new QuitGameUseCase();
         private ResetGameUseCase _resetGameUseCase;
 
+        [Resolve][SerializeField] private TMP_Text _title;
         [Resolve][SerializeField] private Button _resetButton;
         [Resolve][SerializeField] private Button _quitGameButton;
 
@@ -17,6 +22,28 @@ namespace BForBoss
             _resetGameUseCase = resetGameUseCase;
             _quitGameButton.onClick.AddListener(_quitGameUseCase.Execute);
             _resetButton.onClick.AddListener(_resetGameUseCase.Execute);
+        }
+
+        private void OnEnable()
+        {
+            switch (StateManager.Instance.GetState())
+            {
+                case State.Death:
+                    _title.text = GameOverLabel;
+                    break;
+                case State.EndGame:
+                    _title.text = VictoryLabel;
+                    break;
+                default:
+                    return;
+            }
+        }
+
+        private void Awake()
+        {
+            this.PanicIfNullObject(_resetButton, nameof(_resetButton));
+            this.PanicIfNullObject(_quitGameButton, nameof(_quitGameButton));
+            this.PanicIfNullObject(_title, nameof(_title));
         }
 
         private void OnDestroy()
