be1652eea56a96bdfa1ee9f883a7e7b2f69fd0b2
AbelW <unoctium1@gmail.com>
2023-09-06 01:36:16 -0700

    Create jump pad audio

---
+++ Assets/_BForBoss/_Core/Scripts/Environment/JumpPad.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using ECM2.Characters;
+using FMODUnity;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace BForBoss
+{
+    public class JumpPad : MonoBehaviour
+    {
+        [SerializeField] private float _launchImpulse = 15.0f;
+        [SerializeField] private EventReference _jumpAudio;
+
+        [SerializeField] private bool _overrideVerticalVelocity;
+        [SerializeField] private bool _overrideLateralVelocity;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Player"))
+                return;
+
+            Character character = other.GetComponent<Character>();
+            if (character == null)
+                return;
+            
+            RuntimeManager.PlayOneShot(_jumpAudio, transform.position);
+            character.PauseGroundConstraint();
+            character.LaunchCharacter(transform.up * _launchImpulse, _overrideVerticalVelocity, _overrideLateralVelocity);
+        }
+    }
+}
