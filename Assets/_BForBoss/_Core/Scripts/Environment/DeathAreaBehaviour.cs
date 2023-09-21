a740e43b16dc11f470b58f18d8cd06d155426944
abelw <unoctium1@gmail.com>
2023-09-20 18:27:16 -0700

    Audio Fixes (#418)
    * Add audio
    
    * Set up rings
    
    * Set up splash audio
    
    * Disable spatial audio on splash

--- Assets/_BForBoss/_Core/Scripts/Environment/DeathAreaBehaviour.cs
+++ Assets/_BForBoss/_Core/Scripts/Environment/DeathAreaBehaviour.cs
@@ -1,14 +1,20 @@
+using FMODUnity;
 using Perigon.Utility;
 using UnityEngine;
 
 namespace BForBoss
 {
     public class DeathAreaBehaviour : MonoBehaviour
-    {        
+    {
+        [SerializeField]
+        private EventReference _onDeathSFX;
+        
         private void OnCollisionEnter(Collision other)
         {
             if (other.gameObject.CompareTag(TagsAndLayers.Tags.Player))
             {
+                if(!_onDeathSFX.IsNull)
+                    RuntimeManager.PlayOneShot(_onDeathSFX, other.gameObject.transform.position);
                 StateManager.Instance.SetState(State.PreGame);
             }
         }
