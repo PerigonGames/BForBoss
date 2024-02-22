fb4bf0bb82dbc06f84d0a936a76d4236cf47a8e3
Martin <m4rtin.t@gmail.com>
2024-02-22 13:04:09 -0800

    Bug Fix Hookshot

--- Assets/_BForBoss/_Core/Scripts/Character/PlayerHookshotBehaviour.cs
+++ Assets/_BForBoss/_Core/Scripts/Character/PlayerHookshotBehaviour.cs
@@ -1,20 +1,38 @@
 using Perigon.Utility;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace BForBoss
 {
+    /*
+     * TODO - Change Camera -> Player eye pivot
+     * TODO Debug.Log -> Using Logger
+     */
     public class PlayerHookshotBehaviour : MonoBehaviour
     {
-        private const float MIN_DISTANCE_LIMIT = 3;
+
+        [Title("Hookshot Configuration")]
+        [InfoBox("Distance between player and hookshot target")]
         [SerializeField] private float _range = 20f;
         [SerializeField] private float _force = 10f;
+        [InfoBox("Cooldown intervals between hookshots")]
+        [SerializeField] private float _cooldown = 1f;
+        [Title("Hookshot slowdown")]
+        [InfoBox("Slowdown speed")]
+        [SerializeField] private float _slowdownMultiplier = 0.4f;
+        [InfoBox("Distance between player and target before slowing down player during hooktshot")]
+        [SerializeField] private float _distanceBeforeSlowdown = 3;
         private Camera _camera;
 
         private PlayerMovementBehaviour _playerMovement;
-        private bool _canHookShot;
-        private Vector3 _target;
-        private bool _isHookShotting = false;
 
+        private bool _canHookshot = false;
+        private Vector3 _target;
+        private bool _isHookshotting = false;
+        private float _cooldownElapsedTime = 0;
+        
+        private bool CanHookShot => _canHookshot && _cooldownElapsedTime <= 0;
+        
         public void Initialize(
             PGInputSystem input,
             PlayerMovementBehaviour playerMovement)
@@ -30,24 +48,33 @@ namespace BForBoss
 
         private void HookShot(bool didPress)
         {
-            if (_canHookShot)
+            if (CanHookShot && didPress)
             {
                 _playerMovement.LaunchCharacter(target: _target, force: _force);
                 _target = Vector3.zero;
-                _canHookShot = false;
-                _isHookShotting = true;
-                Debug.Log("Start Hookshotting");
+                _canHookshot = false;
+                _isHookshotting = true;
+                _cooldownElapsedTime = _cooldown;
+                Debug.Log("Start Hookshot");
+            }
+        }
+
+        private void Update()
+        {
+            if (_cooldownElapsedTime > 0)
+            {
+                _cooldownElapsedTime -= Time.deltaTime;
             }
         }
 
         private void FixedUpdate()
         {
-            if (Vector3.Distance(transform.position, _target) < MIN_DISTANCE_LIMIT && _isHookShotting)
+            if (Vector3.Distance(transform.position, _target) < _distanceBeforeSlowdown && _isHookshotting)
             {
-                Debug.Log("Stop HookShotting and slow down");
-                _isHookShotting = false;
-                var velocity = _playerMovement.GetVelocity();
-                _playerMovement.SetVelocity(velocity * 0.4f);
+                Debug.Log("Stop Hookshot Slow Down - within slowdown distance");
+                _isHookshotting = false;
+                _playerMovement.SetVelocity(_playerMovement.GetVelocity() * _slowdownMultiplier);
+                return;
             }
 
             if (Physics.Raycast(
@@ -59,18 +86,17 @@ namespace BForBoss
                     queryTriggerInteraction: QueryTriggerInteraction.Collide))
             {
                 _target = raycastHit.transform.position;
-                Debug.Log("Target Set: "+_target);
-                _canHookShot = true;
-            }
-            else
-            {
-                _canHookShot = false;
+                //Debug.Log("Target Set: "+_target);
+                _canHookshot = true;
+                return;
             }
 
-            if (_isHookShotting && _playerMovement.IsOnGround())
+            _canHookshot = false;
+
+            if (_isHookshotting && _playerMovement.IsOnGround())
             {
-                Debug.Log("Stop HookShotting");
-                _isHookShotting = false;
+                Debug.Log("Stop Hookshot - Grounded");
+                _isHookshotting = false;
             }
         }
     }
